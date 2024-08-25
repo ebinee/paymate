e9ef73e07ddc3446dcb55f26a74d3384b2aa3189
tngusdl <tngusdl11.05@gmail.com>
2024-08-25 23:12:35 +0900

    friend_list

--- lib/friend_list.dart
+++ lib/friend_list.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:paymate/header.dart';
 
 class FriendList extends StatefulWidget {
   const FriendList({super.key});
@@ -9,11 +9,351 @@ class FriendList extends StatefulWidget {
 }
 
 class FriendListState extends State<FriendList> {
+  final TextEditingController _idController = TextEditingController();
+
+  final Map<String, String> _idToNameMap = {
+    'mybin': '김예빈',
+    'nenen2ya': '한현비',
+    'sooombb': '이수민',
+    'woooojin': '박우진',
+    'kanghoon': '이강훈',
+    'mammi': '엄마',
+    'daddy': '아빠',
+    'bro': '오빠',
+  };
+
+  final List<Widget> _containers = [];
+
+  @override
+  void dispose() {
+    _idController.dispose();
+    super.dispose();
+  }
+
+  void _showDeleteConfirmationDialog(String name, String id) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: const Color(0xffffffff),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          titlePadding: EdgeInsets.zero,
+          title: const Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              '친구삭제',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xff646464),
+                fontSize: 20,
+              ),
+            ),
+          ),
+          content: Text(
+            '$name 을(를) 삭제하시겠습니까?',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Color(0xff646464),
+              fontSize: 20,
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: const Color(0xffffffff),
+                backgroundColor: const Color(0xff646464),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 12),
+                textStyle: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w100,
+                ),
+              ),
+              child: const Text('취소'),
+              onPressed: () {
+                Navigator.of(context).pop(); // 대화상자 닫기
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: const Color(0xffffffff),
+                backgroundColor: const Color(0xff646464),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 12),
+                textStyle: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w100,
+                ),
+              ),
+              child: const Text('삭제'),
+              onPressed: () {
+                Navigator.of(context).pop(); // 대화상자 닫기
+                _deleteContainer(name, id);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteContainer(String name, String id) {
+    setState(() {
+      _containers.removeWhere((container) {
+        if (container is Container) {
+          final child = container.child as Padding;
+          final row = child.child as Row;
+          final textWidgets = row.children.whereType<Text>().toList();
+          final textName = textWidgets[0].data;
+          final textId = textWidgets[1].data;
+          return textName == name && textId == id;
+        }
+        return false;
+      });
+    });
+  }
+
+  void _addContainer(String name, String id) {
+    setState(() {
+      _containers.add(
+        Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 255, 222, 216),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12.withOpacity(0.2),
+                spreadRadius: 0,
+                blurRadius: 5,
+                offset: const Offset(0, 4),
+              ),
+            ],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const Icon(Icons.person_outline_outlined,
+                    size: 40, color: Color(0xFF646464)),
+                const SizedBox(
+                  width: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 4),
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      color: Color(0xFF646464),
+                      fontSize: 23,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 2),
+                  child: Text(
+                    id,
+                    style: const TextStyle(
+                      color: Color(0xFF646464),
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () {
+                      _showDeleteConfirmationDialog(name, id);
+                    }),
+                const SizedBox(
+                  width: 20,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+      _containers.add(const SizedBox(height: 15));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: Header(
-        headerTitle: '친구 목록',
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('친구 목록'),
+        backgroundColor: const Color(0xFFf2e8da),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const SizedBox(height: 30),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      )
+                    ],
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SizedBox(width: 10),
+                        Icon(Icons.person_outline_outlined,
+                            size: 50, color: Color(0xFF646464)),
+                        SizedBox(width: 15),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 4),
+                          child: Text(
+                            '이수현',
+                            style: TextStyle(
+                              color: Color(0xFF646464),
+                              fontSize: 30,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 2),
+                          child: Text(
+                            'TNGUSDL',
+                            style: TextStyle(
+                              color: Color(0xFF646464),
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 15),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      backgroundColor: const Color(0xffffffff),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      titlePadding: EdgeInsets.zero,
+                      title: const Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Text(
+                          '친구추가',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color(0xff646464),
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      content: Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 0, horizontal: 3.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            TextField(
+                              controller: _idController,
+                              decoration: const InputDecoration(
+                                hintText: '아이디 ID',
+                                contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 2.0, vertical: 8.0),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            foregroundColor: const Color(0xffffffff),
+                            backgroundColor: const Color(0xff646464),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 100, vertical: 12),
+                            textStyle: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w100,
+                            ),
+                          ),
+                          child: const Text('추가',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 15,
+                              )),
+                          onPressed: () {
+                            String enteredId = _idController.text;
+                            Navigator.of(context).pop();
+
+                            String? name = _idToNameMap[enteredId];
+
+                            if (name != null) {
+                              _addContainer(name, enteredId);
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('해당 아이디에 맞는 이름이 없습니다.')));
+                            }
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    '친구추가',
+                    style: TextStyle(
+                      color: Color(0xFF646464),
+                      fontSize: 15,
+                      fontWeight: FontWeight.w100,
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Icon(
+                    Icons.add,
+                    size: 15,
+                    color: Color(0xFF646464),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Expanded(
+              child: ListView(
+                children: _containers,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
